b7e3a466685d2a9383709caf934c16028bb9750f
MoChen-study <773470469@qq.com>
2019-10-14 21:57:08 +0800

    week6

---
+++ week6/code/dyamiclib/test1.c
@@ -0,0 +1,47 @@
+#include "lib.h"
+#include <dlfcn.h>
+int main()
+{
+	int a[5]={0,1,2,3,4};
+	void (*f1)();
+	int (*f2)(),(*f3)();
+	void *handle;
+	handle=dlopen("/home/rlk/Documents/linuxprogram/week6/code/dyamiclib/libdytest.so",RTLD_LAZY);
+	if(!handle){
+	fprintf(stderr,"%s\n",dlerror());
+	exit(1);
+	
+	}
+	f1=dlsym(handle,"urand");
+	if(dlerror()!=NULL){
+	
+	fprintf(stderr,"%s\n",dlerror());
+	exit(1);
+
+	}
+	f2=dlsym(handle,"max");
+	if(dlerror()!=NULL){
+	
+	fprintf(stderr,"%s\n",dlerror());
+	exit(1);
+
+	}
+	f3=dlsym(handle,"sum");
+	if(dlerror()!=NULL){
+	
+	fprintf(stderr,"%s\n",dlerror());
+	exit(1);
+
+	}
+	f1(a,5);
+	
+	printf("max=%d\n",f2(a,5));
+	printf("sum=%d\n",f3(a,5));
+	if(dlclose(handle)<0){
+
+	fprintf(stderr,"%s\n",dlerror());
+	exit(1);
+
+	}
+	return 0;
+}
