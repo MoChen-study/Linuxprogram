1d20cd11f98dcb926345c4f853b00c344c6c1e96
MoChen-study <773470469@qq.com>
2019-11-12 11:10:58 +0800

    week10

--- week10/code/1-3.c
+++ week10/code/1-3.c
@@ -7,6 +7,7 @@ int main()
 {
 
 	int v=99;
+	static int a=20;
 	pid_t pid;
 	pid=vfork();
 	if(pid<0)
@@ -19,24 +20,24 @@ int main()
 
 		g++;
 		v++;
-		printf("child:pid=%d,g=%d,v=%d\n",getpid(),g,v);
-		printf("child:pid=%d,&g=%p,&v=%p\n",getpid(),&g,&v);
-		execl("./1-4","1-4",NULL);
-		static long h=100;
-		printf("parent:pid=%d,g=%d,v=%d\n,&h=%16p",getpid(),g,v,&h);
-		//return 0;
+		a=300;
+		printf("child:pid=%d,g=%d,v=%d,%d\n",getpid(),g,v,a);
+		printf("&g=%p,&v=%p,&a=%p",&g,&v,&a);
+		while(g!=500)
+			printf("wait for parent\n");
 		//return 0;
 		//exit(0);
 		_exit(0);
 	}
 	else
 	{
+		g=500;
 		printf("return value=%d\n",pid);
 		printf("parent:pid=%d,g=%d,v=%d\n",getpid(),g,v);
-		printf("parent:pid=%d,&g=%p,&v=%p\n",getpid(),&g,&v);
-		//printf("&h=%16p\n",&h);
+		printf("&g=%p,&v=%p,&a=%p",&g,&v,&a);
+		return 0;
 		//exit(0);
-		_exit(0);
+		//_exit(0);
 		
 	}
 	return 0;
